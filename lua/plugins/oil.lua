4d687221dbef3d563d4a7a726aa10926afccaa91
zchown <91749140+zchown@users.noreply.github.com>
2024-06-06 17:34:16 -0400

    better oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,20 +1,65 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require('oil').setup({
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require('oil').setup({
+            use_default_keymaps = true,
+            view_options = {
+                show_hidden = true,
+                is_hidden_file = function(name, bufnr)
+                    return vim.startswith(name, ".")
+                end,
+                is_always_hidden = function(name, bufnr)
+                    return false
+                end,
+                natural_order = true,
+                sort = {
+                    { "type", "asc" },
+                    { "name", "asc" },
+                },
+            },
         float = {
             padding = 10,
             max_width = 0,
             max_height = 0,
             border = "rounded",
             win_option = {
-                winblend = 1,
+                winblend = 0,
             },
             override = function(conf)
                 return conf
             end,
+        },
+        preview = {
+            max_width = 0.9,
+            min_width = { 40, 0.4 },
+            width = 60,
+            max_height = 0.9,
+            min_height = { 5, 0.1 },
+            height = 20,
+            border = "rounded",
+            win_options = {
+                winblend = 0,
+            },
+            update_on_cursor_moved = true,
+        },
+        progress = {
+            max_width = 0.9,
+            min_width = { 40, 0.4 },
+            width = nil,
+            max_height = { 10, 0.9 },
+            min_height = { 5, 0.1 },
+            height = nil,
+            border = "rounded",
+            minimized_border = "none",
+            win_options = {
+                winblend = 0,
+            },
+        },
+        -- Configuration for the floating SSH window
+        ssh = {
+            border = "rounded",
         },
     })
   end,
